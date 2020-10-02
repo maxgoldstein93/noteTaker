f1fb50b6143ef1011e0a4afa1374d5a1f7fe2298
Max Goldstein <magoldstein93@gmail.com>
2020-10-02 16:03:23 -0400

    working id

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const path = require("path")
 const express = require('express');
 const fs = require("fs");
-const db =require("./db/db.json");
-const util = require("util")
+let db = require("./db/db.json");
+
 
 
 const app = express()
- 
-const PORT =3636
+
+const PORT = 3636
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,29 +19,46 @@ app.use(express.json());
 // // API Routes
 
 // Get /api/notes
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
   res.json(db);
 });
 
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newNote = req.body;
+  newNote.id = db.length
   console.log(db);
   db.push(newNote);
-  fs.writeFile("./db/db.json",JSON.stringify(db, null, 2),err => {
+  fs.writeFile("./db/db.json", JSON.stringify(db, "utf8", null, 2), err => {
     if (err) throw err;
-    return true;
+    res.json(db);
   });
-  res.json(db);
+
 });
 
-// post api/notes
-// receive json obj from front end
-// writefile
-// return res.status(200).end()
-
 // delete api/notes/:id
+
+// I COULD NOT GET THE APP.Delete to work //
+// ==============================================
+app.delete('/api/notes/:id', function (req, res) {
+  var updatedNote = req.params.id
+  var newNotes = []
+  for (let i = 0; i <= db.length; i++) {
+    if (updatedNote !== db[i].id) {
+      newNotes.push(db[i]);
+
+
+    }
+  }
+  db = newNotes;
+  fs.writeFile("./db/db.json", JSON.stringify(db, "utf8", null, 2), err => {
+    if (err) throw err;
+
+  });
+  res.json(db);
+
+})
 
 // HTML Routes
 
@@ -50,19 +67,19 @@ app.use(express.static('public'))
 
 // Get /notes(.html)
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 // Get * index.html
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-app.listen(PORT, function() {
-    console.log(`Server is running on http://localhost:${PORT}/`);
-  });
+app.listen(PORT, function () {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
 
 
 
